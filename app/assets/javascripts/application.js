0dceef5784359d4c983b7bd783928743c0b82acc
Andrew <anrewp1991@gmail.com>
2019-02-11 15:53:31 +0300

    jquery problem tray4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
 //= require bootstrap
-//= require rails-ujs
-//= require activestorage
 //= require turbolinks
 //= require_tree .
