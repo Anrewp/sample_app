fb739a370b47014d18a1e19e59a5f38b2dcd7cd7
Andrew <anrewp1991@gmail.com>
2019-02-15 13:57:45 +0300

    jquery problem tray21

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require jquery_ujs
+//= require jquery
+//= require rails-ujs
+//= require activestorage
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
